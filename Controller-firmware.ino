8eaaf39ebe7d0df203316e72e826895f1f50d877
aidan <anastasiia.radchenko@volvocars.com>
2023-08-20 08:16:38 +0300

    Bump to the 0.1.2 version

--- Controller-firmware.ino
+++ Controller-firmware.ino
@@ -1,4 +1,4 @@
-// Version 0.1.1 Fix for the ConvertIntToArr (corrupted pow 10)
+// Version 0.1.2 CreateMessageJoystickXY
 
 #include <RH_ASK.h>
 #include <SPI.h>
--- version.md
+++ version.md
@@ -2,3 +2,4 @@
 
 Version 0.1.0 Initial implementation
 Version 0.1.1 Fix for the ConvertIntToArr (corrupted pow 10)
+Version 0.1.2 CreateMessageJoystickXY
